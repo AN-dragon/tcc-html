c9eabdec6f4aaa5dd0f3979c7bdba4f108a13a84
AN-dragon <117469698+AN-dragon@users.noreply.github.com>
2023-08-09 20:17:40 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+require_once('./vendor/autoload.php'); 
+$_title = "Beloto & Beloto Redações | Página Principal" ;
+getTopPage();
+?>
+
+<main>
+    <h1 style="text-align: center;">[Imagem]</h1>
+
+    <p style="text-align: justify; margin: 15px; font-size: 18px;">
+    Na Beloto & Beloto Redações, você pratica e melhora seu desempenho,
+    recebendo correções detalhadas e melhorando seu repertório. 
+    </p>
+
+    <h1 style="text-align: center;">[Imagem]</h1>
+
+</main>
+
+<?php getBottomPage() ?>
